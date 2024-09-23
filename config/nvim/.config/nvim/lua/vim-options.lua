fd9c9f799316356651dc418561b7ad9c87eb193a
DaniilKalts <kaltsdaniil@gmail.com>
2024-09-23 18:39:47 +0500

    06 feat: add centering cursor for moving up and down.

--- config/nvim/.config/nvim/lua/vim-options.lua
+++ config/nvim/.config/nvim/lua/vim-options.lua
@@ -1,11 +1,11 @@
 -- General Settings
-vim.o.number = true         -- Enable line numbers
+vim.o.number = true -- Enable line numbers
 vim.o.relativenumber = true -- Enable relative line numbers
 
 -- Tabs and Indentation
-vim.opt.tabstop = 2      -- Number of spaces per tab
-vim.opt.shiftwidth = 2   -- Number of spaces to use for auto-indentation
-vim.opt.softtabstop = 2  -- Number of spaces when hitting <Tab> in insert mode
+vim.opt.tabstop = 2 -- Number of spaces per tab
+vim.opt.shiftwidth = 2 -- Number of spaces to use for auto-indentation
+vim.opt.softtabstop = 2 -- Number of spaces when hitting <Tab> in insert mode
 vim.opt.expandtab = true -- Convert tabs to spaces
 
 -- Clipboard Integration
@@ -13,3 +13,7 @@ vim.o.clipboard = "unnamedplus" -- Use system clipboard for yank, delete, change
 
 -- Set Space as the leader key
 vim.g.mapleader = " "
+
+-- Create key mappings to move cursor while keeping it centered
+vim.api.nvim_set_keymap("n", "j", "jzz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "kzz", { noremap = true, silent = true })
